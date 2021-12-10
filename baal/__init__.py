34561e74779afcb819c866b30116ca55829633c2
pinarcol <pinarcol95@gmail.com>
2021-12-10 20:41:48 +0300

    Dosyalar düzenlendi

--- baal/__init__.py
+++
@@ -1,5 +0,0 @@
-from .active.dataset import ActiveLearningDataset
-from .modelwrapper import ModelWrapper
-from .utils.log_configuration import set_logger_config
-
-set_logger_config()
--- baal/bayesian/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dropout import Dropout
-from .consistent_dropout import ConsistentDropout
-from .weight_drop import MCDropoutConnectModule
